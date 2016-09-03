e28ec505ab825ea7cad160d6437efb6abd231656
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-09-02 22:36:16 -0400

    Buttons are visibly selectable

--- AmusementParkPassGenerator/ViewController.swift
+++ AmusementParkPassGenerator/ViewController.swift
@@ -47,19 +47,146 @@ class ViewController: UIViewController {
     
     // Instance of Person created from input data
     
-    var person = Person()
+    var entrant = Person()
     
+    // Button Highlighting Variables
+    
+    @IBOutlet weak var secondRowButtonsStackView: UIStackView!
+    
+    var currentlySelectedTypeButton = UIButton()
+    var previouslySelectedTypeButton = UIButton()
+    
+    var buttonPressedPurple = UIColor()
+    var buttonNormalStatePurple = UIColor()
+    
+    var secondRowButtonsArray = [UIButton]()
+    
+    var currentlySelectedSubType = UIButton()
+    var previouslySelectedSubType = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadAccessDeniedSound()
         loadAccessGrantedSound()
-
+        
+        buttonPressedPurple = UIColor(red: 151/255.0, green: 114/255.0, blue: 194/255.0, alpha: 1)
+        buttonNormalStatePurple = UIColor(red: 129/255.0, green: 98/255.0, blue: 164/255.0, alpha: 1.0)
+        
+        secondRowButtonsArray = [r2b1, r2b2, r2b3, r2b4]
+        
+        self.initialViewSetup()
+    }
+    
+    func initialViewSetup() {
+        
+        currentlySelectedTypeButton = guestButton
+        currentlySelectedTypeButton.backgroundColor = buttonPressedPurple
+        
+        setSecondRowButtons(guestButton)
+        
+        currentlySelectedSubType = r2b1
+        currentlySelectedSubType.selected = true
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func entrantTypeSelected(sender: UIButton) {
+        
+        switch sender.tag {
+        
+        case 1:
+            switchHighlight(guestButton)
+            setSecondRowButtons(guestButton)
+        case 2:
+            switchHighlight(employeeButton)
+            setSecondRowButtons(employeeButton)
+        case 3:
+            switchHighlight(contractorButton)
+            setSecondRowButtons(contractorButton)
+        case 4:
+            switchHighlight(vendorButton)
+            setSecondRowButtons(vendorButton)
+        default: break
+        
+        }
+    }
+    
+    @IBAction func entrantSubtypeSelected(sender: UIButton) {
+        
+        switch sender.currentTitle! {
+        
+        // Guest
+        case "Child" :
+           highlightSubtype(r2b1)
+        case "Adult":
+            highlightSubtype(r2b2)
+        case "Senior":
+            highlightSubtype(r2b3)
+        case "Season Pass":
+            highlightSubtype(r2b4)
+        
+        // Employee
+        case "Food Services":
+            highlightSubtype(r2b1)
+        case "Ride Services":
+            highlightSubtype(r2b2)
+        case "Maintenance":
+            highlightSubtype(r2b3)
+        case "Manager" :
+            highlightSubtype(r2b4)
+            
+        default: break
+            
+        }
+    }
+    
+    func highlightSubtype(button: UIButton) {
+        previouslySelectedSubType = currentlySelectedSubType
+        previouslySelectedSubType.selected = false
+        currentlySelectedSubType = button
+        currentlySelectedSubType.selected = true
+    }
+    
+    func setSecondRowButtons(button: UIButton) {
+        switch button {
+            case guestButton :
+                r2b1.setTitle("Child", forState: .Normal)
+                r2b2.setTitle("Adult", forState: .Normal)
+                r2b3.setTitle("Senior", forState: .Normal)
+                r2b4.setTitle("Season Pass", forState: .Normal)
+                
+                secondRowButtonsStackView.hidden = false
+            
+            case employeeButton :
+                r2b1.setTitle("Food Services", forState: .Normal)
+                r2b2.setTitle("Ride Services", forState: .Normal)
+                r2b3.setTitle("Maintenance", forState: .Normal)
+                r2b4.setTitle("Manager", forState: .Normal)
+                
+                secondRowButtonsStackView.hidden = false
+            
+            case contractorButton :
+            
+                secondRowButtonsStackView.hidden = true
+            
+            case vendorButton :
+            
+                secondRowButtonsStackView.hidden = true
+        
+            default: break
+        }
+    }
+    
+    
+    func switchHighlight(button: UIButton) {
+        previouslySelectedTypeButton = currentlySelectedTypeButton
+        previouslySelectedTypeButton.backgroundColor = buttonNormalStatePurple
+        currentlySelectedTypeButton = button
+        button.backgroundColor = buttonPressedPurple
     }
     
     // Helper Methods
@@ -95,7 +222,6 @@ class ViewController: UIViewController {
     func playAccessDeniedSound() {
         AudioServicesPlaySystemSound(accessDeniedSound)
     }
-
 
 }
 
