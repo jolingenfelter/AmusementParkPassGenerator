f562a32b37d4b72d0a2399b61ba73c057ff543d7
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-09-04 10:34:32 -0400

    Buttons highlight appropriately, textfields activate and deactivate as expected

--- AmusementParkPassGenerator/ViewController.swift
+++ AmusementParkPassGenerator/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var r2b2: UIButton!
     @IBOutlet weak var r2b3: UIButton!
     @IBOutlet weak var r2b4: UIButton!
+    @IBOutlet weak var r2b5: UIButton!
     
     @IBOutlet weak var generatePassButton: UIButton!
     @IBOutlet weak var populateDataButton: UIButton!
@@ -44,6 +45,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var stateTextField: UITextField!
     @IBOutlet weak var zipCodeTextField: UITextField!
+    
+    var textFieldArray = [UITextField]()
     
     // Instance of Person created from input data
     
@@ -72,7 +75,8 @@ class ViewController: UIViewController {
         buttonPressedPurple = UIColor(red: 151/255.0, green: 114/255.0, blue: 194/255.0, alpha: 1)
         buttonNormalStatePurple = UIColor(red: 129/255.0, green: 98/255.0, blue: 164/255.0, alpha: 1.0)
         
-        secondRowButtonsArray = [r2b1, r2b2, r2b3, r2b4]
+        secondRowButtonsArray = [r2b1, r2b2, r2b3, r2b4, r2b5]
+        textFieldArray = [DOBTextField, SSNTextField, projectNumberTextField, firstNameTextField, lastNameTextField, companyTextField, streetAddressTextField, cityTextField, stateTextField, zipCodeTextField]
         
         self.initialViewSetup()
     }
@@ -84,15 +88,16 @@ class ViewController: UIViewController {
         
         setSecondRowButtons(guestButton)
         
-        currentlySelectedSubType = r2b1
-        currentlySelectedSubType.selected = true
-        
+        enableTextFields(false)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Select entrant type and subtype
     
     @IBAction func entrantTypeSelected(sender: UIButton) {
         
@@ -101,15 +106,33 @@ class ViewController: UIViewController {
         case 1:
             switchHighlight(guestButton)
             setSecondRowButtons(guestButton)
+            enableTextFields(false)
+            deselectSubtype()
+    
         case 2:
             switchHighlight(employeeButton)
             setSecondRowButtons(employeeButton)
+            enableTextFields(false)
+            deselectSubtype()
+        
         case 3:
             switchHighlight(contractorButton)
             setSecondRowButtons(contractorButton)
+            enableTextFields(true)
+            companyTextField.enabled = false
+            companyTextField.backgroundColor = UIColor.clearColor()
+            SSNTextField.enabled = false
+            SSNTextField.backgroundColor = UIColor.clearColor()
+        
         case 4:
             switchHighlight(vendorButton)
             setSecondRowButtons(vendorButton)
+            enableTextFields(true)
+            projectNumberTextField.enabled = false
+            projectNumberTextField.backgroundColor = UIColor.clearColor()
+            SSNTextField.enabled = false
+            SSNTextField.backgroundColor = UIColor.clearColor()
+        
         default: break
         
         }
@@ -122,27 +145,160 @@ class ViewController: UIViewController {
         // Guest
         case "Child" :
            highlightSubtype(r2b1)
+            childSettings()
+          
         case "Adult":
             highlightSubtype(r2b2)
+            enableTextFields(false)
+        
+            
         case "Senior":
             highlightSubtype(r2b3)
+            seniorSettings()
+            
+            
         case "Season Pass":
             highlightSubtype(r2b4)
+            seasonPassSettings()
+           
         
+        case "VIP":
+            highlightSubtype(r2b5)
+            enableTextFields(false)
+            
         // Employee
         case "Food Services":
             highlightSubtype(r2b1)
+            employeeSettings()
+            
         case "Ride Services":
             highlightSubtype(r2b2)
+            employeeSettings()
+        
         case "Maintenance":
             highlightSubtype(r2b3)
+            employeeSettings()
+
         case "Manager" :
             highlightSubtype(r2b4)
+            employeeSettings()
             
         default: break
             
         }
     }
+    
+    func employeeSettings() {
+        enableTextFields(true)
+        
+        DOBTextField.enabled = true
+        SSNTextField.enabled = true
+        
+        projectNumberTextField.enabled = false
+        projectNumberTextField.backgroundColor = UIColor.clearColor()
+        
+        firstNameTextField.enabled = true
+        lastNameTextField.enabled = true
+        
+        companyTextField.enabled = false
+        companyTextField.backgroundColor = UIColor.clearColor()
+        
+        streetAddressTextField.enabled = true
+        cityTextField.enabled = true
+        stateTextField.enabled = true
+        zipCodeTextField.enabled = true
+        
+    }
+    
+    func seasonPassSettings() {
+        enableTextFields(true)
+        
+        DOBTextField.enabled = false
+        DOBTextField.backgroundColor = UIColor.clearColor()
+        
+        SSNTextField.enabled = false
+        SSNTextField.backgroundColor = UIColor.clearColor()
+        
+        projectNumberTextField.enabled = false
+        projectNumberTextField.backgroundColor = UIColor.clearColor()
+        
+        firstNameTextField.enabled = true
+        lastNameTextField.enabled = true
+        
+        companyTextField.enabled = false
+        companyTextField.backgroundColor = UIColor.clearColor()
+        
+        streetAddressTextField.enabled = true
+        cityTextField.enabled = true
+        stateTextField.enabled = true
+        zipCodeTextField.enabled = true
+        
+    }
+    
+    func seniorSettings() {
+        enableTextFields(true)
+        
+        DOBTextField.enabled = true
+        
+        SSNTextField.enabled = false
+        SSNTextField.backgroundColor = UIColor.clearColor()
+        
+        projectNumberTextField.enabled = false
+        projectNumberTextField.backgroundColor = UIColor.clearColor()
+        
+        firstNameTextField.enabled = true
+        lastNameTextField.enabled = true
+        
+        companyTextField.enabled = false
+        companyTextField.backgroundColor = UIColor.clearColor()
+        
+        streetAddressTextField.enabled = false
+        streetAddressTextField.backgroundColor = UIColor.clearColor()
+        
+        cityTextField.enabled = false
+        cityTextField.backgroundColor = UIColor.clearColor()
+        
+        stateTextField.enabled = false
+        stateTextField.backgroundColor = UIColor.clearColor()
+        
+        zipCodeTextField.enabled = false
+        zipCodeTextField.backgroundColor = UIColor.clearColor()
+        
+    }
+    
+    func childSettings() {
+        enableTextFields(true)
+        for textField in textFieldArray {
+            if textField != DOBTextField {
+                textField.enabled = false
+                textField.backgroundColor = UIColor.clearColor()
+            } else {
+                textField.enabled = true
+            }
+        }
+    }
+    
+    func enableTextFields(isEnabled: Bool) {
+        for textField in textFieldArray {
+            if isEnabled == false {
+                textField.enabled = false
+                textField.backgroundColor = UIColor.clearColor()
+            } else {
+                textField.enabled = true
+                textField.backgroundColor = UIColor.whiteColor()
+            }
+        }
+    }
+    
+    func deselectSubtype() {
+        for button in secondRowButtonsArray {
+            if button.selected == true {
+                button.selected = false
+            }
+        }
+    }
+    
+    // Button helper methods
     
     func highlightSubtype(button: UIButton) {
         previouslySelectedSubType = currentlySelectedSubType
@@ -158,7 +314,10 @@ class ViewController: UIViewController {
                 r2b2.setTitle("Adult", forState: .Normal)
                 r2b3.setTitle("Senior", forState: .Normal)
                 r2b4.setTitle("Season Pass", forState: .Normal)
+                r2b5.setTitle("VIP", forState: .Normal)
                 
+                r2b5.hidden = false
+                secondRowButtonsStackView.addArrangedSubview(r2b5)
                 secondRowButtonsStackView.hidden = false
             
             case employeeButton :
@@ -167,6 +326,8 @@ class ViewController: UIViewController {
                 r2b3.setTitle("Maintenance", forState: .Normal)
                 r2b4.setTitle("Manager", forState: .Normal)
                 
+                r2b5.hidden = true
+                secondRowButtonsStackView.removeArrangedSubview(r2b5)
                 secondRowButtonsStackView.hidden = false
             
             case contractorButton :
